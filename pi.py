af0d9269f73711b72a65187d62de9242ee42e279
Kubokouki <k.kubo.24.7.1544@4nonome.com>
2025-02-10 20:17:37 +0900

    python_intor 訂正

--- pi.py
+++ pi.py
@@ -5,5 +5,10 @@ text = """
 
 # TODO
 
-print(list(map(len, ["How", "I","want","a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving",
-    "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard"])))
+clean_text = text.replace(",", "").replace(".", "")
+
+words = clean_text.split()
+
+result = ''.join(str(len(word)) for word in words)
+
+print(result)
