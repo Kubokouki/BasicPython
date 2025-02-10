af560c03b709381a6524dd7f7ab425084e54e552
Kubokouki <k.kubo.24.7.1544@4nonome.com>
2025-02-10 20:16:25 +0900

    制御構文の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,10 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+s = 0
+i = 0
+h = (1/2)*3.141592/100 
+for i in range(101):
+    if i != 0:
+        s = s + (h/2*(sin((i-1)*h)+sin(i*h)))
+print("面積は"+ str(s) + "です。")
